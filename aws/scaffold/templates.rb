0dd61cfcfa38dc628adaf1689cb0f3eea61f4010
Matt Conway <matt@conwaysplace.com>
2018-07-25 11:22:10 -0400

    ops recipes should force override default to keep them minimal.  Add atmos-support to ops recipes

--- aws/scaffold/templates.rb
+++ aws/scaffold/templates.rb
@@ -16,10 +16,10 @@ if ! config_present?('config/atmos.yml', 'environments.ops.recipes.bootstrap',
              ['atmos-variables', 'atmos-bootstrap']
 end
 
-if ! config_present?('config/atmos.yml', 'environments.ops.recipes.default',
-                     ['atmos-variables', 'atmos-permissions'])
-  add_config 'config/atmos.yml', 'environments.ops.recipes.default',
-             ['atmos-variables', 'atmos-permissions']
+if ! config_present?('config/atmos.yml', 'environments.ops.recipes.^default',
+                     ['atmos-variables', 'atmos-permissions', 'atmos-support'])
+  add_config 'config/atmos.yml', 'environments.ops.recipes.^default',
+             ['atmos-variables', 'atmos-permissions', 'atmos-support']
 end
 
 if ! config_present?('config/atmos.yml', 'org')
